c538276b8d7f712b039ae5489cf38c0f36c80eac
Cristian Tacu <167236843+cr1sti91@users.noreply.github.com>
2024-09-07 18:59:04 +0300

    Update AllAdditionalAndLibraries.h

--- include/AllAdditionalAndLibraries.h
+++ include/AllAdditionalAndLibraries.h
@@ -17,7 +17,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include <SFMl/Audio.hpp>
+#include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
 #include "auxFunctions.h"
